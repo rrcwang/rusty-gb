1bc296b9f23304f4ccdd9ea45cce6ce0ae948420
rrcwang <15862033+rrcwang@users.noreply.github.com>
2021-02-21 20:29:25 -0800

    CPU flags done. Updated & reorg'd regs unit tests.

--- src/registers.rs
+++ src/registers.rs
@@ -1,3 +1,8 @@
+// registers.rs
+// Implements the CPU registers
+
+use std::fmt;
+
 pub struct Registers {
     // 8 bit registers
     a: u8,
@@ -29,8 +34,15 @@ pub enum Register16b {
     BC,
     DE,
     HL,
-    SP,
-    PC,
+    SP, // stack pointer
+    PC, // program counter
+}
+
+pub enum Flag {
+    Z, // zero flag
+    N, // add/sub
+    H, // half carry
+    C, // carry flag
 }
 
 impl Registers {
@@ -50,15 +62,15 @@ impl Registers {
     }
 
     /// Sets the value in one of the 8 bit CPU registers
-    /// 
+    ///
     /// # Arguments
-    /// 
+    ///
     /// * `reg` - The register to be written to.
     /// * `value` - u8 value written to the specified register
     pub fn set_8b_reg(&mut self, reg: &Register8b, value: u8) {
         match reg {
             Register8b::A => self.a = value,
-            Register8b::F => self.f = value,
+            Register8b::F => self.f = value, // should never be written to directly?
             Register8b::B => self.b = value,
             Register8b::C => self.c = value,
             Register8b::D => self.d = value,
@@ -69,14 +81,14 @@ impl Registers {
     }
 
     /// Retrieves value stored in one of the 8 bit CPU registers.
-    /// 
+    ///
     /// # Arguments
-    /// 
+    ///
     /// * `reg` - The register to be read from.
-    /// 
+    ///
     /// # Return value
-    /// 
-    /// u8 type.
+    ///
+    /// `u8` type.
     pub fn get_8b_reg(&self, reg: &Register8b) -> u8 {
         match reg {
             Register8b::A => self.a,
@@ -91,15 +103,15 @@ impl Registers {
     }
 
     /// Sets the value in one of the 16 bit CPU registers
-    /// 
+    ///
     /// # Arguments
-    /// 
+    ///
     /// * `reg` - The register to be written to.
     /// * `value` - u16 value written to the specified register
-    /// 
+    ///
     /// # Return value
-    /// 
-    /// u16 type.
+    ///
+    /// `u16` type.
     pub fn set_16b_reg(&mut self, reg: &Register16b, value: u16) {
         match reg {
             Register16b::AF => {
@@ -126,16 +138,15 @@ impl Registers {
             Register16b::PC => self.pc = value,
         }
     }
-    
     /// Retrieves value stored in one of the 8 bit CPU registers.
-    /// 
+    ///
     /// # Arguments
-    /// 
+    ///
     /// * `reg` - The register to be read from.
-    /// 
+    ///
     /// # Return value
-    /// 
-    /// u16 type.
+    ///
+    /// `u16` type.
     pub fn get_16b_reg(&self, reg: &Register16b) -> u16 {
         match reg {
             Register16b::AF => Registers::combine_high_low_bytes(self.a, self.f),
@@ -157,62 +168,236 @@ impl Registers {
         let low: u8 = (value & 0x00FF) as u8;
         (high, low)
     }
+
+    /// Sets desired flag in CPU register F to 1.
+    ///
+    /// # Arguments
+    ///
+    /// * `flag` - The flag to be set. One of `Flag::{Z, N, H, C}`.
+    pub fn set_flag(&mut self, flag: &Flag) {
+        match flag {
+            Flag::Z => self.f |= 0b_1000_0000,
+            Flag::N => self.f |= 0b_0100_0000,
+            Flag::H => self.f |= 0b_0010_0000,
+            Flag::C => self.f |= 0b_0001_0000,
+        }
+    }
+
+    /// Clears desired flag in CPU register F to 0.
+    ///
+    /// # Arguments
+    ///
+    /// * `flag` - The flag to be cleared. One of `Flag::{Z, N, H, C}`.
+    pub fn clear_flag(&mut self, flag: &Flag) {
+        match flag {
+            Flag::Z => self.f &= 0b_0111_1111,
+            Flag::N => self.f &= 0b_1011_1111,
+            Flag::H => self.f &= 0b_1101_1111,
+            Flag::C => self.f &= 0b_1110_1111,
+        }
+    }
+
+    /// Returns the valued stored in one of the CPU flags.
+    ///
+    /// # Arguments
+    ///
+    /// * `flag` - The flag to be read from. One of Flag::{Z, N, H, C}.
+    ///
+    /// # Return value
+    ///
+    /// * `bool` type where
+    /// | return value  | flag state           |
+    /// |:-------------:|:--------------------:|
+    /// | `true`        | `register.flag == 1` |
+    /// | `false `      | `register.flag == 0` |
+    pub fn get_flag(&self, flag: &Flag) -> bool {
+        let bit: u8 = match flag {
+            Flag::Z => (self.f & 0b_1000_0000) >> 7,
+            Flag::N => (self.f & 0b_0100_0000) >> 6,
+            Flag::H => (self.f & 0b_0010_0000) >> 5,
+            Flag::C => (self.f & 0b_0001_0000) >> 4,
+        };
+
+        match bit {
+            0 => false,
+            1 => true,
+            _ => panic!("Nonesense result returned for CPU flag! (neither 0/1)"),
+        }
+    }
 }
 
-// unit tests for Registers registers
+impl fmt::Display for Registers {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+          f,
+          "A:{:02X} F:{:04b} \
+           B:{:02X} C:{:02X} \
+           D:{:02X} E:{:02X} \
+           H:{:02X} L:{:02X} \
+           PC:{:04X} \
+           SP:{:04X}",
+          self.a, self.f, self.b, self.c, self.d, self.e, self.h, self.l, self.pc, self.sp
+        )
+    }
+}
+
+/// unit tests for CPU registers
+/// 
+/// NOTE: test functions named as `register_{TYPE}_{TEST}...`, where `TEST` refers to the functionality/cases tested for
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn register_set_get_8b() {
-        let registers_8b = vec![Register8b::A, Register8b::F, Register8b::B, Register8b::C, Register8b::D, Register8b::E, Register8b::H, Register8b::L];
+    /// test helper module
+    mod common {
+        use super::super::*;
+        pub fn all_registers_8b() -> Vec<Register8b> {
+            vec![
+                Register8b::A,
+                Register8b::F,
+                Register8b::B,
+                Register8b::C,
+                Register8b::D,
+                Register8b::E,
+                Register8b::H,
+                Register8b::L,
+            ]
+        }
 
-        let mut cpu = Registers::new();
+        pub fn all_registers_16b() -> Vec<Register16b> {
+            vec![
+                Register16b::AF,
+                Register16b::BC,
+                Register16b::DE,
+                Register16b::HL,
+                Register16b::SP,
+                Register16b::PC,
+            ]
+        }
+
+        pub fn all_flags() -> Vec<Flag> {
+            vec![
+                Flag::Z,
+                Flag::N,
+                Flag::H,
+                Flag::C,
+            ]
+        }
+    }
+
+    #[test]
+    fn register_8b_set_get() {
+        let registers_8b = common::all_registers_8b();
+
+        let mut registers = Registers::new();
 
         for reg in &registers_8b {
-            cpu.set_8b_reg(reg, 0x1)
+            registers.set_8b_reg(reg, 0x1)
         }
 
         for reg in &registers_8b {
-            assert_eq!(1u8, cpu.get_8b_reg(reg))
+            assert_eq!(1u8, registers.get_8b_reg(reg))
         }
     }
 
     #[test]
-    fn register_init_zero_8b() {
-        let registers_8b = vec![Register8b::A, Register8b::F, Register8b::B, Register8b::C, Register8b::D, Register8b::E, Register8b::H, Register8b::L];
+    fn register_8b_init_zero_get() {
+        let registers_8b = common::all_registers_8b();
 
-        let cpu = Registers::new();
+        let registers = Registers::new();
 
         for reg in &registers_8b {
-            assert_eq!(0u8, cpu.get_8b_reg(reg));
+            assert_eq!(0u8, registers.get_8b_reg(reg));
         }
     }
 
     #[test]
-    fn register_init_zero_16b() {
-        let registers_16b = vec![Register16b::AF, Register16b::BC, Register16b::DE, Register16b::HL, Register16b::SP, Register16b::PC];
+    fn register_16b_init_zero_get() {
+        let registers_16b = common::all_registers_16b();
 
-        let cpu = Registers::new();
+        let registers = Registers::new();
 
         for reg in &registers_16b {
-            assert_eq!(0u16, cpu.get_16b_reg(reg));
+            assert_eq!(0u16, registers.get_16b_reg(reg));
         }
     }
 
     #[test]
-    fn register_set_get_16b() {
-        let registers_16b = vec![Register16b::AF, Register16b::BC, Register16b::DE, Register16b::HL, Register16b::SP, Register16b::PC];
+    fn register_16b_set_get() {
+        let registers_16b = common::all_registers_16b();
 
-        let mut cpu = Registers::new();
+        let mut registers = Registers::new();
 
         for reg in &registers_16b {
-            cpu.set_16b_reg(reg, 0xF0F0u16)
+            registers.set_16b_reg(reg, 0xF0F0u16)
         }
 
         for reg in &registers_16b {
-            assert_eq!(0xF0F0u16, cpu.get_16b_reg(reg));
+            assert_eq!(0xF0F0u16, registers.get_16b_reg(reg));
         }
     }
+
+    #[test]
+    fn register_16b_set_8b_get() {
+        let mut registers = Registers::new();
+
+        let registers_16b = common::all_registers_16b();
+
+        for reg_16b in &registers_16b {
+            registers.set_16b_reg(reg_16b, 0xF0F0u16)
+        }
+
+        let registers_8b = common::all_registers_8b();
+        for reg_8b in &registers_8b {
+            assert_eq!(0xF0u8, registers.get_8b_reg(reg_8b));
+        }
+    }
+
+    #[test]
+    fn register_8b_set_16b_get() {
+        let mut registers = Registers::new();
+        
+        let registers_8b = common::all_registers_8b();
+
+        for reg_8b in &registers_8b {
+            registers.set_8b_reg(reg_8b, 0xF0u8);
+        }
+
+        let registers_16b = common::all_registers_16b();
+
+        for reg_16b in &registers_16b[0..4] { // exclude SP, PC registers
+            println!("{}", registers);
+            assert_eq!(0xF0F0u16, registers.get_16b_reg(reg_16b));
+        }
+
+    }
+
+    #[test]
+    fn register_flag_init_zero_get() {
+        let flags = common::all_flags();
+
+        let mut registers = Registers::new();
+
+        for flag in &flags {
+            assert_eq!(false, registers.get_flag(flag));
+        }
+    }
+
+    #[test]
+    fn register_flag_set_clear() {
+        let flags = common::all_flags();
+
+        let mut registers = Registers::new();
+        // set all flags and check for true
+        for flag in &flags {
+            registers.set_flag(flag);
+            assert_eq!(true, registers.get_flag(flag));
+        }
+        // clear all flags and check for false
+        for flag in &flags {
+            registers.clear_flag(flag);
+            assert_eq!(false, registers.get_flag(flag));
+        }
+    }
+
 }
