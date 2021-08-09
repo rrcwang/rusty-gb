37963e55c16e5bd65df0a268b0305cbf655ea976
rrcwang <15862033+rrcwang@users.noreply.github.com>
2021-08-09 02:34:26 -0700

    refactor: move modules into lib, add clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,24 @@
+#![warn(
+    clippy::pedantic,
+    clippy::multiple_crate_versions,
+    clippy::cognitive_complexity,
+    clippy::missing_const_for_fn,
+    clippy::needless_borrow,
+    clippy::redundant_pub_crate,
+    clippy::string_lit_as_bytes,
+    clippy::use_self,
+    clippy::useless_let_if_seq,
+    rust_2018_idioms,
+    future_incompatible,
+)]
 
+#![allow(
+    clippy::missing_errors_doc,
+    clippy::match_bool,
+    clippy::map_err_ignore,
+)]
+
+pub mod cpu;
+pub mod memory;
+pub mod registers;
+pub mod utils;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-mod cpu;
-mod memory;
-mod registers;
-mod utils;
-
 fn main() {
-    let mut cpu = cpu::Cpu::new();
-    cpu.fetch_and_execute();
+    
 }
